a3c99c2231be3bb1ba0f36ae93522a5e4f7ac6f5
bzsome <chao@DESKTOP-CHAO>
2018-08-13 18:12:53 +0800

    重修命名项目模块

---
+++ server-netty/src/main/java/com/chao/server/ChatAndWebSocket.java
@@ -0,0 +1,35 @@
+package com.chao.server;
+
+import com.chao.server.chatServer.ChatServer;
+import com.chao.server.webServer.WebSocketServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChatAndWebSocket {
+    private final static Logger logger = LoggerFactory.getLogger(ChatAndWebSocket.class);
+
+    public static void main(String[] args) {
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    new ChatServer(9902).run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    new WebSocketServer(9901).run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+    }
+}
