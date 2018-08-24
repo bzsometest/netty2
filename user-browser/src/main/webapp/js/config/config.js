32836e4b8b26cb83e4e176c82db4378e1bff1ddb
bzsome <chao@DESKTOP-CHAO>
2018-08-24 18:09:40 +0800

    提升页面加载速度，采用在线cdn。 去掉时间转换插件。

--- user-browser/src/main/webapp/js/config/config.js
+++ user-browser/src/main/webapp/js/config/config.js
@@ -4,17 +4,17 @@
  */
 
 //服务器api接口地址
-var api_path = "http://gobang04.bzchao.com/oauth";
+var oauth_path = "http://localhost:8080/oauth";
 var netty_path = "gobang04.bzchao.com:9901/ws";
 _settings = {
     gobangDown: "http://t.cn/RrXswvP",
     jdkDown: "http://down-www.newasp.net/pcdown/soft/yh/jre1.8x64.7z",
     api: {
-        user: api_path + "/user/user",//用户操作接口(CRUD)
-        login: api_path + "/user/login",//用户登录接口地址
-        logout: api_path + "/user/logout",//注销登录接口地址
-        register: api_path + "/user/register",//用户注册接口(CRUD)
-        check: api_path + "/check",//信息后端检验
+        user: oauth_path + "/user/user",//用户操作接口(CRUD)
+        login: oauth_path + "/user/login",//用户登录接口地址
+        logout: oauth_path + "/user/logout",//注销登录接口地址
+        register: oauth_path + "/user/register",//用户注册接口(CRUD)
+        check: oauth_path + "/user/check",//信息后端检验
     },
     html: {
         index: "index.html",
--- user-browser/src/main/webapp/js/user.center.js
+++ user-browser/src/main/webapp/js/user.center.js
@@ -14,6 +14,25 @@ function show_user_center() {
     $(user_div).find("#email").text(userData.email);
     $(user_div).find("#sign").text(userData.sign);
     //使用时间格式化插件格式化时间
-    var time = moment(userData.regTime).format('YYYY-MM-DD HH:mm:ss');
+    var time = new Date().Format('YYYY-MM-DD HH:mm:ss');
     $(user_div).find("#reg_time").text(time);
+}
+
+Date.prototype.Format = function (fmt) { //author: meizz
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "D+": this.getDate(), //日
+        "H+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(Y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
 }
